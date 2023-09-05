8ab515a18e2d66e065004bc7241e46037f65c8f7
Rose Wright <rosesophiewright@gmail.com>
2023-09-04 22:36:40 -0500

    Buttons CSS

--- app/_conponents/Future.jsx
+++ app/_conponents/Future.jsx
@@ -1,10 +1,10 @@
 export default function Future () {
     return (
-        <div className="flex flex-col justify-center bg-silver h-[90dvh]">
-            <h1 className="text-8xl mx-[7dvw] font-bold">Building the future, one line of code at a time</h1>
-            <div className="text-4xl mx-[7dvw] font-bold flex">
-                <span className="p-5 m-5 bg-red">button</span>
-                <span className="p-5 m-5 bg-red">button</span>
+        <div className="flex flex-col justify-center bg-silver h-[90dvh] text-purple">
+            <h1 className="text-8xl mx-[10dvw] font-bold">Building the future, one line of code at a time</h1>
+            <div className="text-4xl mx-[10dvw] font-bold flex">
+                <span className="px-10 py-3 m-3 rounded-lg bg-red text-silver border-4 border-red">Case Studies</span>
+                <span className="px-16 py-3 m-3 rounded-lg bg-silver text-purple border-4">Contact</span>
             </div>
         </div>
     )
