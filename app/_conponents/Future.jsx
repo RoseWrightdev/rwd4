62f218d55d7bac4e8bc8df43b86d33dce2a66f1f
Rose Wright <rosesophiewright@gmail.com>
2023-09-04 22:30:07 -0500

    Future CSS setup

---
+++ app/_conponents/Future.jsx
@@ -0,0 +1,11 @@
+export default function Future () {
+    return (
+        <div className="flex flex-col justify-center bg-silver h-[90dvh]">
+            <h1 className="text-8xl mx-[7dvw] font-bold">Building the future, one line of code at a time</h1>
+            <div className="text-4xl mx-[7dvw] font-bold flex">
+                <span className="p-5 m-5 bg-red">button</span>
+                <span className="p-5 m-5 bg-red">button</span>
+            </div>
+        </div>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -1,11 +1,12 @@
 import Nav from "./_conponents/Nav"
 import ProgressBar from "./_conponents/ProgressBar"
-
+import Future from "./_conponents/Future"
 export default function Home() {
   return (
     <>
       <ProgressBar/>
       <Nav/>
+      <Future/>
       <div className="h-[200vh]"></div>
     </>    
   )
